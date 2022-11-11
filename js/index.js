f0d7f68c94e966cf2a6b5f13cb50be211991f6d4
MariaRiobo <mmrioboarrizabalaga@udesa.edu.ar>
2022-11-11 09:49:01 -0300

    form

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
-fetch("https://api.themoviedb.org/3/movie/76341?api_key=bfec0622d489778cd408f2f5942ce52d")
+
+let apikey= "bfec0622d489778cd408f2f5942ce52d"
+let api= `https://api.themoviedb.org/3/movie/76341?api_key=${apikey}`
+
+// Fetch para pelis populares
+fetch(api)
    .then(function(response){
         return response.json();
    })
    .then(function(data){
-    return data;
+        return data;
     })
    .catch(function(error){
-    return error;
+        return error;
     })
+// formulario
+  
+
+
+
+
