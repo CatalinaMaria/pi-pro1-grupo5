1c23a2a7359a927a55eade61d089c123b4b0b72f
CatalinaMaria <catalina.arzubiaga@gmail.com>
2022-11-23 14:14:31 -0300

    detalle de genero

--- js/detalle-genero.js
+++ js/detalle-genero.js
@@ -18,6 +18,8 @@ let url = "";
 let titulo = document.querySelector(".tituloResultado");
 let section = document.querySelector(".cajapadre1");
 
+
+
 // buscador
 form.addEventListener('submit', function(e){
      e.preventDefault();
@@ -34,37 +36,11 @@ form.addEventListener('submit', function(e){
 })
 
 
-// Detalle genero
+// Detalle genero de series
 
-if (variableTipo == "pelicula") {
-  url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${variableId}`;
-  titulo.innerText = `${variableGenero} : Peliculas`;
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
-      let info = data.results;
-      let peliculaFav = "";
-      for (let i = 0; i < 5; i++) {
-        console.log(info[i].title);
-        peliculaFav = `<article > <a href="./detalle-pelicula.html?id=${info[i].id}"> <img src="https://image.tmdb.org/t/p/w500/${info[i].poster_path}"
-        alt=" foto poster ${info[i].title}"> </a>
-        <p class="nombrePeli">${info[i].title}</p>
-        <p>  Fecha de estreno: ${info[i].release_date} </p>
-        <a href="./detalle-pelicula.html?id=${info[i].id}">
-           <button type="" class="verMas">Ver mas</button>
-            </a>
-        </article>`
-        section.innerHTML += peliculaFav;
-      }
-    })
-    .catch(function (error) {
-      console.log("Error: " + error);
-    });
-} else {
-  url = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&with_genres=${variableId}`;
+
+
+url = `https://api.themoviedb.org/3/discover/tv?api_key=${apiKey}&with_genres=${variableId}`;
   console.log(url)
   titulo.innerText = `${variableGenero}:Series`;
   fetch(url)
@@ -92,6 +68,3 @@ if (variableTipo == "pelicula") {
     .catch(function (error) {
       console.log("Error: " + error);
     });
-}
-
-
